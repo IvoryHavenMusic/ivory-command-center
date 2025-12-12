1507cb87c53264632658e843da96c442c611fd6e
IvoryHavenMusic <gordon.noseworthy@gmail.com>
2025-12-12 15:49:14 -0330

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
 // supabase.js  (pure ES module)
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const SUPABASE_URL = 'https://YOUR-PROJECT-REF.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_PUBLIC_ANON_KEY'; // anon/public key only
+const SUPABASE_URL = 'https://wlzjymtugtovmitkkjsw';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indsemp5bXR1Z3Rvdm1pdGtranN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMyNjk4MzgsImV4cCI6MjA3ODg0NTgzOH0.vnHmV1GA--NXLLMWgetoS_AqZFYbHg2gWzMmQAncaME'; // anon/public key only
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
